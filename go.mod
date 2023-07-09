c2068d9573f9581e370f9db86505ad9b9a240cd3
alekseiKromski <aleksei.kromski@outlook.com>
2023-07-09 22:09:38 +0300

    main: added code for checking ping availability

--- go.mod
+++ go.mod
@@ -3,7 +3,16 @@ module alekseikormski.com/server-status-monitoring
 go 1.19
 
 require (
-	github.com/gosuri/uilive v0.0.4 // indirect
+	github.com/go-ping/ping v1.1.0
+	github.com/gosuri/uilive v0.0.4
+)
+
+require (
+	github.com/fatih/color v1.15.0 // indirect
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
+	golang.org/x/net v0.0.0-20210316092652-d523dce5a7f4 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.6.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,20 +2,112 @@ package main
 
 import (
 	"fmt"
+	"github.com/fatih/color"
+	"github.com/go-ping/ping"
 	"github.com/gosuri/uilive"
 	"time"
 )
 
+type status string
+
+const (
+	FAILED  status = "FAILED"
+	SUCCESS status = "SUCCESS"
+	INIT    status = "INIT"
+)
+
+var (
+	info    = color.New(color.FgWhite, color.BgCyan)
+	success = color.New(color.FgWhite, color.BgGreen)
+	failed  = color.New(color.FgWhite, color.BgRed)
+)
+
+type application struct {
+	link      string
+	ip        string
+	status    status
+	lastCheck time.Time
+}
+
 func main() {
+	applications := []*application{
+		&application{
+			link:   "kromline.ee",
+			status: INIT,
+		},
+		&application{
+			link:   "admin.kromline.ee",
+			status: INIT,
+		},
+		&application{
+			link:   "alekseikromski.com",
+			status: INIT,
+		},
+		&application{
+			link:   "jenkins.alekseikromski.com",
+			status: INIT,
+		},
+		&application{
+			link:   "docker.alekseikromski.com",
+			status: INIT,
+		},
+		&application{
+			link:   "blog.alekseikromski.com",
+			status: INIT,
+		},
+	}
 	writer := uilive.New()
 	// start listening for updates and render
 	writer.Start()
 
-	for i := 0; i <= 1000000; i++ {
-		fmt.Fprintf(writer, "Downloading.. (%d/%d) GB\n", i, 100)
-		time.Sleep(time.Millisecond * 5)
+	go func() {
+		for {
+			for _, app := range applications {
+				pinger, err := ping.NewPinger(app.link)
+				if err != nil {
+					panic(err)
+				}
+
+				pinger.Count = 1
+				pinger.Timeout = 2 * time.Second
+				pinger.Run() // blocks until finished
+				stats := pinger.Statistics()
+
+				if stats.PacketLoss > 0.0 {
+					app.status = FAILED
+				} else {
+					app.status = SUCCESS
+				}
+				app.ip = stats.IPAddr.String()
+				app.lastCheck = time.Now()
+				time.Sleep(1 * time.Second)
+			}
+			time.Sleep(5 * time.Second)
+		}
+	}()
+
+	for {
+		render(applications, writer)
+		time.Sleep(500 * time.Millisecond)
 	}
 
-	fmt.Fprintln(writer, "Finished: Downloaded 100GB")
 	writer.Stop() // flush and stop rendering
+}
+
+func render(applications []*application, writer *uilive.Writer) {
+	content := ""
+	for _, app := range applications {
+		status := ""
+		switch app.status {
+		case INIT:
+			status = info.Sprintf("%s", app.status)
+		case SUCCESS:
+			status = success.Sprintf("%s", app.status)
+		case FAILED:
+			status = failed.Sprintf("%s", app.status)
+		}
+		content += fmt.Sprintf("[%s]: %s [%s] (%.fs ago)\n", status, app.link, app.ip, time.Now().Sub(app.lastCheck).Seconds())
+	}
+	fmt.Fprintf(writer, content)
+
 }
